46ea608dc7b1e9b8aaea7ee23b6899b07679a5a7
ishahruk96 <ishahruk96@gmail.com>
2019-10-21 18:01:11 +0600

    frist commit

---
+++ MvcMultilingual/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MvcMultilingual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMultilingual.Controllers
+{
+    public class HomeController : MyBaseController
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(RegistrationModel r)
+        {
+            return View(r);
+        }
+
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new SiteLanguages().SetLanguage(lang);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
